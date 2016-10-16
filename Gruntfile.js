eeb32ab1a981126b0b39a961decd94d4cecf68f0
Snehal Parate <sparate@my.harrisburgu.edu>
2016-10-16 13:02:08 -0400

    Updated file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,18 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        // uglify: {
-        //     build: {
-        //         src: 'css/style.css',
-        //         dest: 'css/build/style.css'
-        //     }
-        // },
+        uglify: {
+            build: {
+                src: 'js/bootstrap.js',
+                dest: 'js/bootstrap.min.js'
+            }
+        },
         imagemin: {                          // Task
             dynamic: {                         // Another target
               files: [{
                 expand: true,                  // Enable dynamic expansion
                 cwd: 'images/',                   // Src matches are relative to this path
-                src: ['*.JPG'],   // Actual patterns to match
+                src: ['feed_reader.JPG', 'resume.JPG', 'movie_trailor.JPG'],   // Actual patterns to match
                 dest: 'images/dist/'                  // Destination path prefix
               }]
             }
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                 },{
                     name: 'large',
                     width: 640,
-                    height: 436
+                    height: 480
                 },{
                     name: "large",
                     width: 1024,
@@ -42,38 +42,42 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'images/dist',
-                    src: ['*.JPG'],
+                    src: ['feed_reader.JPG', 'resume.JPG', 'movie_trailor.JPG'],
                     dest: 'images/resp/'
                 }]
             }
-        },
-        // copy: {
-        //     dev: {
-        //         files: [{
-        //             expand: true,
-        //             src: ['**', 'images/*.*'],
-        //             cwd: 'src/',
-        //             dest: 'dist/'
-        //         }]
-        //     }
-        // }
-        // cssmin: {
-        //   css: {
-        //     files: {
-        //       'css/minified/portfolio-main.min.css': ['css/portfolio-main.css']
-        //     }
-        //   }
-        // }
+        }
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    src: ['**', 'images/*.*'],
+                    cwd: 'src/',
+                    dest: 'dist/'
+                }]
+            }
+        }
+        cssmin: {
+          css: {
+            files: {
+              'css/portfolio-main.min.css': ['css/portfolio-main.css']
+            }
+          }
+        }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-responsive-images');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    // grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['imagemin', 'responsive_images']);
+    grunt.registerTask('default', ['cssmin',
+                                    'responsive_images',
+                                    'uglify',
+                                    'imagemin',
+                                    'copy']);
 };
